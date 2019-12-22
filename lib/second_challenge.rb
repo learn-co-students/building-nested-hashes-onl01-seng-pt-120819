388a5cb9bb8e941ef5ccee2c74b23818164e308c
Martin Chaplin <martin.chaplin44@gmail.com>
2019-12-22 19:03:59 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,9 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {:patriarch, :matriarch and :hero/:heroine
       #your key/value pairs here!
       }, 
-    :capulet => {
+    :capulet => {:patriarch, :matriarch and :hero/:heroine
       #your key/value pairs here!
       }}
 
